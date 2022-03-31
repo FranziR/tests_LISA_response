d2806d107c38a16e8e6d401000a3b582555c86c9
FranziR <39301391+FranziR@users.noreply.github.com>
2022-03-31 12:53:41 +0200

    Add files via upload

--- tests/TDI/test_TDI.m
+++ tests/TDI/test_TDI.m
@@ -1,78 +1,53 @@
 clear; clc; clf
 %% Read in FastGB data
-[YY, YYSL, parameters, tDS]=read_data();
+[YY, parameters, tDS]=read_data();
 T=max(tDS)+(tDS(2)-tDS(1));
-freq0=floor(parameters(6).*T)./T;
+freq0=floor(parameters(:,6).*T)./T;
 %% Generate analytic model data
 [pr_,ps_,n_,L]=lisa_geometry_modified(tDS);
-[YY_, YYSL_,E,k,h,fctr]=lisa_gen_events_modified(tDS,pr_,ps_,n_,L,parameters,freq0,T);
-% YY_=fftshift(fft(YY_,[],1),1);
-% YYSL_=fftshift(fft(YYSL_,[],1),1);
+[~, YY_,E,k,h,fctr]=lisa_gen_events_modified(tDS,pr_,ps_,n_,L,parameters,freq0,T);
 %% Compare
 M=numel(parameters)/8;
 freqs=(-numel(tDS)/2:numel(tDS)/2-1)./T;
 
-for ii=1:3
-    clf
-    err=norm(YY_(:,ii)-YY(:,ii),'fro')/norm(YY_(:,ii),'fro');
+for jj=1:M
+    for ii=1:3
+        clf
+        err=norm(YY_(:,ii,jj)-YY(:,ii,jj),'fro')/norm(YY_(:,ii,jj),'fro');
 
-    subplot(3,1,1)
-    plot(freqs, real(YY(:,ii))); hold on
-    plot(freqs, real(YY_(:,ii)),'--');
-    plot(freqs, real(YY(:,ii)-YY_(:,ii)),'g:');
-    title(num2str(err))
+        subplot(3,1,1)
+        plot(freqs, real(YY(:,ii,jj))); hold on
+        plot(freqs, real(YY_(:,ii,jj)),'--');
+        plot(freqs, real(YY(:,ii,jj)-YY_(:,ii,jj)),'g:');
+        title(num2str(err))
 
-    subplot(3,1,2)
-    plot(freqs, imag(YY(:,ii))); hold on
-    plot(freqs, imag(YY_(:,ii)),'--');
-    plot(freqs, imag(YY(:,ii)-YY_(:,ii)),'g:');
+        subplot(3,1,2)
+        plot(freqs, imag(YY(:,ii,jj))); hold on
+        plot(freqs, imag(YY_(:,ii,jj)),'--');
+        plot(freqs, imag(YY(:,ii,jj)-YY_(:,ii,jj)),'g:');
 
-    subplot(3,1,3)
-    plot(freqs, abs(YY(:,ii))); hold on
-    plot(freqs, abs(YY_(:,ii)),'--');
-    plot(freqs, abs(YY(:,ii)-YY_(:,ii)),'g:');
+        subplot(3,1,3)
+        plot(freqs, abs(YY(:,ii,jj))); hold on
+        plot(freqs, abs(YY_(:,ii,jj)),'--');
+        plot(freqs, abs(YY(:,ii,jj)-YY_(:,ii,jj)),'g:');
 
-    drawnow
-end
-
-
-for ii=1:3
-    clf
-    err=norm(YYSL_(:,ii)-YYSL(:,ii),'fro')/norm(YYSL_(:,ii),'fro');
-
-    subplot(3,1,1)
-    plot(freqs, real(YYSL(:,ii))); hold on
-    plot(freqs, real(YYSL_(:,ii)),'--');
-    plot(freqs, real(YYSL(:,ii)-YYSL_(:,ii)),'g:');
-    title(num2str(err))
-
-    subplot(3,1,2)
-    plot(freqs, imag(YYSL(:,ii))); hold on
-    plot(freqs, imag(YYSL_(:,ii)),'--');
-    plot(freqs, imag(YYSL(:,ii)-YYSL_(:,ii)),'g:');
-
-    subplot(3,1,3)
-    plot(freqs, abs(YYSL(:,ii))); hold on
-    plot(freqs, abs(YYSL_(:,ii)),'--');
-    plot(freqs, abs(YYSL(:,ii)-YYSL_(:,ii)),'g:');
-
-    drawnow
+        drawnow
+    end
 end
 %% functions
-function [YY, YYSL, parameters, t]=read_data()
+function [YY, parameters, t]=read_data()
     Tobs=33554432;
     N=1024; 
     dt=Tobs/N;
     t=(0:dt:Tobs-dt)';
 
     file_path = fileparts(mfilename('fullpath'));
-    M=1; 
 
-    fileID = fopen('parameters.txt','r');
-    tmp=fscanf(fileID,'%f %f %f %f %f %f %f %f');
+    fileID = fopen(fullfile(file_path,'parameters.bin')); 
+    tmp=fread(fileID,'double');
+    parameters=permute(reshape(tmp, 8, []),[2,1]);
     fclose(fileID);
-    parameters=permute(reshape(tmp, 8, 10),[2, 1]);
-    parameters=parameters(1,:);
+    M=numel(parameters)/8;
 
     YY=zeros(numel(t),3,M);
     
@@ -93,25 +68,4 @@ function [YY, YYSL, parameters, t]=read_data()
     tmp=reshape(tmp, [], M);
     YY(:,3,:)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
     fclose(fileID);
-    
-    YYSL=zeros(numel(t),3,M);
-    
-    fileID = fopen(fullfile(file_path,'XSL.bin')); 
-    tmp=fread(fileID,'double');
-    tmp=reshape(tmp, [], M);
-    YYSL(:,1,:)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
-    fclose(fileID);   
-    
-    fileID = fopen(fullfile(file_path,'YSL.bin')); 
-    tmp=fread(fileID,'double');
-    tmp=reshape(tmp, [], M);
-    YYSL(:,2,:)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
-    fclose(fileID);
-    
-    fileID = fopen(fullfile(file_path,'ZSL.bin')); 
-    tmp=fread(fileID,'double');
-    tmp=reshape(tmp, [], M);
-    YYSL(:,3,:)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
-    fclose(fileID);
-
 end
--- tests/zwischenergebnisse/test_zwischenergebnisse.m
+++ tests/zwischenergebnisse/test_zwischenergebnisse.m
@@ -1,8 +1,8 @@
 clear; clc; clf
 %% Read in FastGB data
-[phaseShift, xi, n, ep, ec, dApsi, parameters, tDS]=read_data();
+[phaseShift, xi, n,dApsi, parameters, tDS]=read_data();
 T=max(tDS)+(tDS(2)-tDS(1));
-freq0=floor(parameters(6).*T)./T;
+freq0=floor(parameters(:,6).*T)./T;
 %% Generate analytic model data
 [pr_,ps_,n_,L]=lisa_geometry_modified(tDS);
 [YY_, dApsi_, phaseShift_, xi_, ep_, ec_]=lisa_gen_events_modified(tDS,pr_,ps_,n_,L,parameters,freq0);
@@ -12,29 +12,29 @@ freqs=(-numel(tDS)/2:numel(tDS)/2-1)./T;
 psFFT=fftshift(fft(phaseShift,[],1),1);
 psFFT_=fftshift(fft(phaseShift_,[],1),1);
 
-dApsi_=dApsi_(:,1:3,1);
+for jj=1:2
+    for ii=1:3
+        clf
+        err=norm(dApsi_(:,ii,jj)-dApsi(:,ii,jj),'fro')/norm(dApsi_(:,ii,jj),'fro');
 
-for ii=1:3
-    clf
-    err=norm(dApsi_(:,ii)-dApsi(:,ii),'fro')/norm(dApsi_(:,ii),'fro');
+        subplot(3,1,1)
+        plot(tDS, real(dApsi(:,ii,jj))); hold on
+        plot(tDS, real(dApsi_(:,ii,jj)),'--');
+        plot(tDS, real(dApsi(:,ii,jj)-dApsi_(:,ii,jj)),'g:');
+        title(num2str(err))
 
-    subplot(3,1,1)
-    plot(tDS, real(dApsi(:,ii))); hold on
-    plot(tDS, real(dApsi_(:,ii)),'--');
-    plot(tDS, real(dApsi(:,ii)-dApsi_(:,ii)),'g:');
-    title(num2str(err))
+        subplot(3,1,2)
+        plot(tDS, imag(dApsi(:,ii,jj))); hold on
+        plot(tDS, imag(dApsi_(:,ii,jj)),'--');
+        plot(tDS, imag(dApsi(:,ii,jj)-dApsi_(:,ii,jj)),'g:');
 
-    subplot(3,1,2)
-    plot(tDS, imag(dApsi(:,ii))); hold on
-    plot(tDS, imag(dApsi_(:,ii)),'--');
-    plot(tDS, imag(dApsi(:,ii)-dApsi_(:,ii)),'g:');
+        subplot(3,1,3)
+        plot(tDS, abs(dApsi(:,ii,jj))); hold on
+        plot(tDS, abs(dApsi_(:,ii,jj)),'--');
+        plot(tDS, abs(dApsi(:,ii,jj)-dApsi_(:,ii,jj)),'g:');
 
-    subplot(3,1,3)
-    plot(tDS, abs(dApsi(:,ii))); hold on
-    plot(tDS, abs(dApsi_(:,ii)),'--');
-    plot(tDS, abs(dApsi(:,ii)-dApsi_(:,ii)),'g:');
-
-    drawnow
+        drawnow
+    end
 end
 
 for jj=1:3
@@ -88,108 +88,101 @@ for ii=1:3
     drawnow
 end
 
-for ii=1:3
-    clf
-    err=norm(phaseShift(:,ii)-phaseShift_(:,ii),'fro')/norm(phaseShift(:,ii),'fro');
+for jj=1:5
+    for ii=1:3
+        clf
+        err=norm(phaseShift(:,ii,jj)-phaseShift_(:,ii,jj),'fro')/norm(phaseShift(:,ii,jj),'fro');
 
-    subplot(3,2,1)
-    plot(tDS, real(phaseShift(:,ii))); hold on
-    plot(tDS, real(phaseShift_(:,ii)),'--');
-    plot(tDS, real(phaseShift(:,ii)-phaseShift_(:,ii)),'g:');
-    title(num2str(err))
+        subplot(3,2,1)
+        plot(tDS, real(phaseShift(:,ii,jj))); hold on
+        plot(tDS, real(phaseShift_(:,ii,jj)),'--');
+        plot(tDS, real(phaseShift(:,ii,jj)-phaseShift_(:,ii,jj)),'g:');
+        title(num2str(err))
 
-    subplot(3,2,3)
-    plot(tDS, imag(phaseShift(:,ii))); hold on
-    plot(tDS, imag(phaseShift_(:,ii)),'--');
-    plot(tDS, imag(phaseShift(:,ii)-phaseShift_(:,ii)),'g:');
+        subplot(3,2,3)
+        plot(tDS, imag(phaseShift(:,ii,jj))); hold on
+        plot(tDS, imag(phaseShift_(:,ii,jj)),'--');
+        plot(tDS, imag(phaseShift(:,ii,jj)-phaseShift_(:,ii,jj)),'g:');
 
-    subplot(3,2,5)
-    plot(tDS, abs(phaseShift(:,ii))); hold on
-    plot(tDS, abs(phaseShift_(:,ii)),'--');
-    plot(tDS, abs(phaseShift(:,ii)-phaseShift_(:,ii)),'g:');
+        subplot(3,2,5)
+        plot(tDS, abs(phaseShift(:,ii,jj))); hold on
+        plot(tDS, abs(phaseShift_(:,ii,jj)),'--');
+        plot(tDS, abs(phaseShift(:,ii,jj)-phaseShift_(:,ii,jj)),'g:');
 
-    subplot(3,2,2)
-    plot(freqs, real(psFFT(:,ii))); hold on
-    plot(freqs, real(psFFT_(:,ii)),'--');
-    plot(freqs, real(psFFT(:,ii)-psFFT_(:,ii)),'g:');
-    xlim([-1e-6 1e-6])
-    title(num2str(err))
+        subplot(3,2,2)
+        plot(freqs, real(psFFT(:,ii,jj))); hold on
+        plot(freqs, real(psFFT_(:,ii,jj)),'--');
+        plot(freqs, real(psFFT(:,ii,jj)-psFFT_(:,ii,jj)),'g:');
+        xlim([-1e-6 1e-6])
+        title(num2str(err))
 
-    subplot(3,2,4)
-    plot(freqs, imag(psFFT(:,ii))); hold on
-    plot(freqs, imag(psFFT_(:,ii)),'--');
-    plot(freqs, imag(psFFT(:,ii)-psFFT_(:,ii)),'g:');
-    xlim([-1e-6 1e-6])
+        subplot(3,2,4)
+        plot(freqs, imag(psFFT(:,ii,jj))); hold on
+        plot(freqs, imag(psFFT_(:,ii,jj)),'--');
+        plot(freqs, imag(psFFT(:,ii,jj)-psFFT_(:,ii,jj)),'g:');
+        xlim([-1e-6 1e-6])
 
-    subplot(3,2,6)
-    plot(freqs, abs(psFFT(:,ii))); hold on
-    plot(freqs, abs(psFFT_(:,ii)),'--');
-    plot(freqs, abs(psFFT(:,ii)-psFFT_(:,ii)),'g:');
-    xlim([-1e-6 1e-6])
+        subplot(3,2,6)
+        plot(freqs, abs(psFFT(:,ii,jj))); hold on
+        plot(freqs, abs(psFFT_(:,ii,jj)),'--');
+        plot(freqs, abs(psFFT(:,ii,jj)-psFFT_(:,ii,jj)),'g:');
+        xlim([-1e-6 1e-6])
 
-    drawnow
+        drawnow
+    end
 end
 %% functions
-function [phaseShift, xi, n, ep, ec, dApsi, parameters, t]=read_data()
+function [phaseShift, xi, n, dApsi, parameters, t]=read_data()
     Tobs=33554432;
     N=1024; 
     dt=Tobs/N;
     t=(0:dt:Tobs-dt)';
 
     file_path = fileparts(mfilename('fullpath'));
-    M=1; 
 
-    fileID = fopen('parameters.txt','r');
-    tmp=fscanf(fileID,'%f %f %f %f %f %f %f %f');
+    fileID = fopen(fullfile(file_path,'parameters.bin')); 
+    tmp=fread(fileID,'double');
+    parameters=permute(reshape(tmp, 8, []),[2,1]);
     fclose(fileID);
-    parameters=permute(reshape(tmp, 8, 10),[2, 1]);
-    parameters=parameters(1,:);
-%     parameters(1)=pi/2;
-%     parameters(2)=pi/2;
-%     parameters(3)=pi/2;
-%     parameters(4)=1;
-%     parameters(5)=0;
-%     parameters(6)=1e-3+1e-6;
-%     parameters(7)=0;
-%     parameters(8)=0;
+    M=numel(parameters)/8;
 
-    phaseShift=zeros(numel(t),3);
+    phaseShift=zeros(numel(t),3,M);
     
     fileID = fopen(fullfile(file_path,'phaseShift12.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    phaseShift(:,1)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
+    phaseShift(:,1,:)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
     fclose(fileID);
     
     fileID = fopen(fullfile(file_path,'phaseShift23.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    phaseShift(:,2)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
+    phaseShift(:,2,:)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
     fclose(fileID);
     
     fileID = fopen(fullfile(file_path,'phaseShift31.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    phaseShift(:,3)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
+    phaseShift(:,3,:)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
     fclose(fileID);
 
-    xi=zeros(numel(t),3);   
+    xi=zeros(numel(t),3,M);   
     fileID = fopen(fullfile(file_path,'xi1.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    xi(:,1)=tmp;
+    xi(:,1,:)=tmp;
     fclose(fileID);
     
     fileID = fopen(fullfile(file_path,'xi2.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    xi(:,2)=tmp;
+    xi(:,2,:)=tmp;
     fclose(fileID);
     
     fileID = fopen(fullfile(file_path,'xi3.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    xi(:,3)=tmp;
+    xi(:,3,:)=tmp;
     fclose(fileID);
 
     n=zeros(numel(t),3,3);
@@ -197,57 +190,45 @@ function [phaseShift, xi, n, ep, ec, dApsi, parameters, t]=read_data()
     fileID = fopen(fullfile(file_path,'n12.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    n(:,1,1)=tmp(1:3:end-2);
-    n(:,2,1)=tmp(2:3:end-1);
-    n(:,3,1)=tmp(3:3:end);
+    n(:,1,1)=tmp(1:3:end-2,1);
+    n(:,2,1)=tmp(2:3:end-1,1);
+    n(:,3,1)=tmp(3:3:end,1);
     fclose(fileID);
 
     fileID = fopen(fullfile(file_path,'n23.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    n(:,1,2)=tmp(1:3:end-2);
-    n(:,2,2)=tmp(2:3:end-1);
-    n(:,3,2)=tmp(3:3:end);
+    n(:,1,2)=tmp(1:3:end-2,1);
+    n(:,2,2)=tmp(2:3:end-1,1);
+    n(:,3,2)=tmp(3:3:end,1);
     fclose(fileID);
 
     fileID = fopen(fullfile(file_path,'n31.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    n(:,1,3)=tmp(1:3:end-2);
-    n(:,2,3)=tmp(2:3:end-1);
-    n(:,3,3)=tmp(3:3:end);
+    n(:,1,3)=tmp(1:3:end-2,1);
+    n(:,2,3)=tmp(2:3:end-1,1);
+    n(:,3,3)=tmp(3:3:end,1);
     fclose(fileID);
 
-    fileID = fopen(fullfile(file_path,'eplus.bin'));
-    tmp=fread(fileID,'double');
-    tmp=reshape(tmp, 3, 3);
-    ep=tmp;
-    fclose(fileID);
-
-    fileID = fopen(fullfile(file_path,'ecross.bin'));
-    tmp=fread(fileID,'double');
-    tmp=reshape(tmp, 3, 3);
-    ec=tmp;
-    fclose(fileID);
-
-    dApsi=zeros(numel(t),3);
+    dApsi=zeros(numel(t),3,M);
     
     fileID = fopen(fullfile(file_path,'dApsi12.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    dApsi(:,1)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
+    dApsi(:,1,:)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
     fclose(fileID);
     
     fileID = fopen(fullfile(file_path,'dApsi23.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    dApsi(:,2)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
+    dApsi(:,2,:)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
     fclose(fileID);
     
     fileID = fopen(fullfile(file_path,'dApsi31.bin')); 
     tmp=fread(fileID,'double');
     tmp=reshape(tmp, [], M);
-    dApsi(:,3)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
+    dApsi(:,3,:)=tmp(1:2:end-1,:)+1i*tmp(2:2:end,:);
     fclose(fileID);
 
 end
